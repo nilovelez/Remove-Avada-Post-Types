6c71469461d925122aa531f00339f82c38fd138b
Nilo Velez <nilovelez@gmail.com>
2017-06-01 22:10:13 +0200

    Initial commit

---
+++ Remove-Avada-Post-Types.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Remove Avada Post Types
+Description: Plugin to deactivate Avada's portfolio and FAQ's post types and thumbnail sizes
+Version:     0.1
+Author:      Nilo Velez
+License:     GPL2
+*/
