a2164a1c1c4b529a20f16f3e7b75ff176c3ccdac
Nilo Velez <nilovelez@gmail.com>
2017-06-01 23:18:46 +0200

    more fixes

--- Remove-Avada-Post-Types.php
+++ Remove-Avada-Post-Types.php
@@ -19,14 +19,12 @@ add_action( 'init', 'remove_avada_image_sizes');
 
 
 function unregister_avada_post_types(){
-	unregister_taxonomy_for_object_type( 'portfolio_category', 'avada_portfolio' );
-	unregister_taxonomy_for_object_type( 'portfolio_skills', 'avada_portfolio' );
-	unregister_taxonomy_for_object_type( 'portfolio_tags', 'avada_portfolio' );
+	unregister_taxonomy ('portfolio_category' );
+	unregister_taxonomy ('portfolio_skills' );
+	unregister_taxonomy ('portfolio_tags' );
 	unregister_post_type('avada_portfolio');
-	unregister_taxonomy_for_object_type( 'faq_category', 'avada_faq' );
-	unregister_post_type('avada_faq');
-
-	
+	unregister_taxonomy ('faq_category' );
+	unregister_post_type('avada_faq');	
 }
 add_action( 'wp_loaded', 'unregister_avada_post_types');
 
